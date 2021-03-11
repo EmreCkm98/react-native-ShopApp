059dc2d0909387786c3ee0ca3926c58cb6b3c9e4
Emre Çakmak <emreckmk98@hotmail.com>
2021-03-11 23:28:02 +0300

    app is loaded

---
+++ components/shop/OrderItem.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+import Colors from "../../constants/Colors";
+import CartItem from "./CartItem";
+import Cart from "../../components/UI/Cart";
+
+const OrderItem = (props) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  return (
+    <Cart style={styles.orderItem}>
+      <View style={styles.summary}>
+        <Text style={styles.totalAmount}>${props.amount}</Text>
+        <Text style={styles.date}>{props.date}</Text>
+      </View>
+      <Button
+        color={Colors.primary}
+        title={showDetails ? "Hide Details" : "Show Details"}
+        onPress={() => {
+          setShowDetails((prevState) => !prevState);
+        }}
+      />
+      {showDetails && (
+        <View style={styles.detailItems}>
+          {props.items.map((cartItem) => (
+            <CartItem
+              key={cartItem.productId}
+              quantity={cartItem.quantity}
+              amount={cartItem.sum}
+              title={cartItem.productTitle}
+            />
+          ))}
+        </View>
+      )}
+    </Cart>
+  );
+};
+
+const styles = StyleSheet.create({
+  orderItem: {
+    margin: 20,
+    padding: 10,
+    alignItems: "center",
+  },
+  summary: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 15,
+  },
+  totalAmount: {
+    fontFamily: "open-sans-bold",
+    fontSize: 16,
+  },
+  date: {
+    fontFamily: "open-sans",
+    fontSize: 16,
+    color: "#888",
+  },
+  detailItems: {
+    width: "100%",
+  },
+});
+
+export default OrderItem;
